22759713d454d983da66f9341223c706a32ab5d6
Gustavo Sthel <gustavosthel@gmail.com>
2024-03-16 20:44:11 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,9 @@
 package application;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +16,8 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gamil.com", d01, 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.creatSellerDao();
 		
 		System.out.println(seller);
 	}
