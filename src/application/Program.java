553e6d4bbde107a5cefcedee0ffd35ae438bb5dc
Gustavo Sthel <gustavosthel@gmail.com>
2024-03-16 20:27:32 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		LocalDate d01 = LocalDate.now();
+		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gamil.com", d01, 3000.0, obj);
+		
+		System.out.println(seller);
 	}
 
 }
